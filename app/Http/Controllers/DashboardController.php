7284273a579b09a044038fb5651d6510f2f239d0
Danuartha10109013 <odanuartha@gmail.com>
2024-12-21 12:57:42 +0700

    perbaruan12345

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,7 +15,20 @@ class DashboardController extends Controller
 
         // Total revisions (sum of `no_revisi` column)
         $totalRevisions = Material::count();
-        return view('pages.pegawai.index',compact('totalJobcards','totalRevisions'));
+
+        // Monthly Jobcard Data for Chart
+        $monthlyJobcards = JobCardM::selectRaw('MONTH(date) as month, COUNT(*) as count')
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get();     
+
+        // Prepare data for Chart.js
+        $monthlyJobcardLabels = $monthlyJobcards->pluck('month')->map(function ($month) {
+            return \Carbon\Carbon::create()->month($month)->format('F'); // Convert month numbers to names
+        });
+        $monthlyJobcardData = $monthlyJobcards->pluck('count');
+        return view('pages.pegawai.index',compact('totalJobcards','totalRevisions','monthlyJobcardLabels',
+            'monthlyJobcardData'));
     }
     public function admin()
     {
